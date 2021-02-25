77b3d61539ab52e6a4df4e4d34064dd138676011
suryakencana08 <sury4.832@gmail.com>
2021-02-25 15:09:53 +0800

    fix logo

--- lrvl_SellMart/resources/views/index.blade.php
+++ lrvl_SellMart/resources/views/index.blade.php
@@ -52,7 +52,7 @@ https://templatemo.com/tm-558-klassy-cafe
                     <nav class="main-nav">
                         <!-- ***** Logo Start ***** -->
                         <a href="index.html" class="logo">
-                            <img src="assets/images/S2.png" align="klassy cafe html template">
+                            <img src="assets/img/SM2.png" align="klassy cafe html template">
 
                         </a>
                         <!-- ***** Logo End ***** -->
@@ -289,15 +289,18 @@ https://templatemo.com/tm-558-klassy-cafe
                                 <div class="thumb-lg member-thumb mx-auto"><img src="https://bootdey.com/img/Content/avatar/avatar7.png" class="rounded-circle img-thumbnail" alt="profile-image"></div>
                                 <div class="">
                                     <h4>I Putu Adi Perdana Utama</h4>
-                                    <p class="text-muted">@Founder <span>| </span><span><a href="#" class="text-pink">webperdagangan</a></span></p>
+                                    <p class="text-muted">Ketua <span>| </span><span><a href="#" class="text-pink">@SellMart-Web</a></span></p>
                                 </div>
                                 <div class="desc">
-                                    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                    consequat.</span>
+                                    <p align="justify">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. 
+                                    </p>
                                 </div>  
-                                <a href="https://api.whatsapp.com/send?phone=6285804086639"><button type="button" class="btn btn-danger mt-3 btn-rounded waves-effect w-md waves-light">Message Now</button></a>
+                                <a href="https://api.whatsapp.com/send?phone=6285804086639"><button type="button" class="btn btn-secondary mt-3 btn-rounded waves-effect w-md waves-light">Message Now</button></a>
                                 <br>
                                 <hr>
                                 <ul class="social-network social-circle">
@@ -314,73 +317,21 @@ https://templatemo.com/tm-558-klassy-cafe
                             <div class="member-card pt-2 pb-2">
                                 <div class="thumb-lg member-thumb mx-auto"><img src="https://bootdey.com/img/Content/avatar/avatar7.png" class="rounded-circle img-thumbnail" alt="profile-image"></div>
                                 <div class="">
-                                    <h4>I Made Dita Yoga Radisa</h4>
-                                    <p class="text-muted">@Programmer <span>| </span><span><a href="#" class="text-pink">webperdagangan</a></span></p>
-                                </div>
-                                <div class="desc">
-                                    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                    consequat.</span>
-                                </div>  
-                                <a href="https://api.whatsapp.com/send?phone=6285854323229"><button type="button" class="btn btn-danger mt-3 btn-rounded waves-effect w-md waves-light">Message Now</button></a>
-                                <br>
-                                <hr>
-                                <ul class="social-network social-circle">
-                                  <li><a href="https://instagram.com/dittayoga" class="icoInstagram" title="Instagram"><i class="fa fa-instagram"></i></a></li>
-                                  <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-                                  <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- end col -->
-                    <div class="col-lg-4">
-                        <div class="text-center card-box">
-                            <div class="member-card pt-2 pb-2">
-                                <div class="thumb-lg member-thumb mx-auto"><img src="https://bootdey.com/img/Content/avatar/avatar7.png" class="rounded-circle img-thumbnail" alt="profile-image"></div>
-                                <div class="">
-                                    <h4>I Made Adi Wira Surya Kencana</h4>
-                                    <p class="text-muted">@Webdesigner <span>| </span><span><a href="#" class="text-pink">webperdagangan</a></span></p>
-                                </div>
-                                <div class="desc">
-                                    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                    consequat.</span>
-                                </div>  
-                                <a href="https://api.whatsapp.com/send?phone=6289697448955"><button type="button" class="btn btn-danger mt-3 btn-rounded waves-effect w-md waves-light">Message Now</button></a>
-                                <br>
-                                <hr>
-                                <ul class="social-network social-circle">
-                                  <li><a href="https://instagram.com/erx.surya" class="icoInstagram" title="Instagram"><i class="fa fa-instagram"></i></a></li>
-                                  <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-                                  <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- end col -->
-                </div>
-                <div class=""></div>
-                <!-- end row -->
-                <div class="row">
-                    <div class="col-lg-4">
-                        <div class="text-center card-box">
-                            <div class="member-card pt-2 pb-2">
-                                <div class="thumb-lg member-thumb mx-auto"><img src="https://bootdey.com/img/Content/avatar/avatar7.png" class="rounded-circle img-thumbnail" alt="profile-image"></div>
-                                <div class="">
                                     <h4>I Gede Arista Winkayana</h4>
-                                    <p class="text-muted">@Webdesigner <span>| </span><span><a href="#" class="text-pink">webperdagangan</a></span></p>
+                                    <p class="text-muted">Admin <span>| </span><span><a href="#" class="text-pink">@SellMart-Web</a></span></p>
                                 </div>
                                 <div class="desc">
-                                    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                    consequat.</span>
+                                    <p align="justify">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur.
+                                    </p>
                                 </div>  
-                                <a href="https://api.whatsapp.com/send?phone=6281236034522"><button type="button" class="btn btn-danger mt-3 btn-rounded waves-effect w-md waves-light">Message Now</button></a>
-                                <br><hr>
+                                <a href="https://api.whatsapp.com/send?phone=6281236034522"><button type="button" class="btn btn-secondary mt-3 btn-rounded waves-effect w-md waves-light">Message Now</button></a>
+                                <br>
+                                <hr>
                                 <ul class="social-network social-circle">
                                   <li><a href="https://instagram.com/winka_bob" class="icoInstagram" title="Instagram"><i class="fa fa-instagram"></i></a></li>
                                   <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
@@ -395,16 +346,82 @@ https://templatemo.com/tm-558-klassy-cafe
                             <div class="member-card pt-2 pb-2">
                                 <div class="thumb-lg member-thumb mx-auto"><img src="https://bootdey.com/img/Content/avatar/avatar7.png" class="rounded-circle img-thumbnail" alt="profile-image"></div>
                                 <div class="">
-                                    <h4>Aldi Suryanto</h4>
-                                    <p class="text-muted">@Webdesigner <span>| </span><span><a href="#" class="text-pink">webperdagangan</a></span></p>
+                                    <h4>I Made Adi Wira Surya Kencana</h4>
+                                    <p class="text-muted">Front End <span>| </span><span><a href="#" class="text-pink">@SellMart-Web</a></span></p>
                                 </div>
                                 <div class="desc">
-                                    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                    consequat.</span>
+                                    <p align="justify">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur.
+                                    </p>
                                 </div>  
-                                <a href="https://api.whatsapp.com/send?phone=6285851413429"><button type="button" class="btn btn-danger mt-3 btn-rounded waves-effect w-md waves-light">Message Now</button></a>
+                                <a href="https://api.whatsapp.com/send?phone=6289697448955"><button type="button" class="btn btn-secondary mt-3 btn-rounded waves-effect w-md waves-light">Message Now</button></a>
+                                <br>
+                                <hr>
+                                <ul class="social-network social-circle">
+                                  <li><a href="https://instagram.com/erx.surya" class="icoInstagram" title="Instagram"><i class="fa fa-instagram"></i></a></li>
+                                  <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                                  <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end col -->
+                </div>
+
+                
+
+                <!-- end row -->
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="text-center card-box">
+                            <div class="member-card pt-2 pb-2">
+                                <div class="thumb-lg member-thumb mx-auto"><img src="https://bootdey.com/img/Content/avatar/avatar7.png" class="rounded-circle img-thumbnail" alt="profile-image"></div>
+                                <div class="">
+                                    <h4>I Made Dita Yoga Radisa</h4>
+                                    <p class="text-muted">Back End<span> | </span><span><a href="#" class="text-pink">@SellMart-Web</a></span></p>
+                                </div>
+                                <div class="desc">
+                                    <p align="justify">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. 
+                                    </p>
+                                </div>  
+                                <a href="https://api.whatsapp.com/send?phone=6285854323229"><button type="button" class="btn btn-secondary mt-3 btn-rounded waves-effect w-md waves-light">Message Now</button></a>
+                                <br><hr>
+                                <ul class="social-network social-circle">
+                                  <li><a href="https://instagram.com/dittayoga " class="icoInstagram" title="Instagram"><i class="fa fa-instagram"></i></a></li>
+                                  <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                                  <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end col -->
+                    <div class="col-lg-4">
+                        <div class="text-center card-box">
+                            <div class="member-card pt-2 pb-2">
+                                <div class="thumb-lg member-thumb mx-auto"><img src="https://bootdey.com/img/Content/avatar/avatar7.png" class="rounded-circle img-thumbnail" alt="profile-image"></div>
+                                <div class="">
+                                    <h4>Aldi Suryanto</h4>
+                                    <p class="text-muted">Pemasaran<span> | </span><span><a href="#" class="text-pink">@SellMart-Web</a></span></p>
+                                </div>
+                                <div class="desc">
+                                    <p align="justify">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur.
+                                    </p>
+                                </div>  
+                                <a href="https://api.whatsapp.com/send?phone=6285851413429"><button type="button" class="btn btn-secondary mt-3 btn-rounded waves-effect w-md waves-light">Message Now</button></a>
                                 <br>
                                 <hr>
                                 <ul class="social-network social-circle">
@@ -516,7 +533,7 @@ https://templatemo.com/tm-558-klassy-cafe
                 </div>
                 <div class="col-lg-4">
                     <div class="logo">
-                        <a href="index.html"><img src="assets/images/S2.png" alt=""></a>
+                        <a href="index.html"><img src="assets/img/SM3.png" alt=""></a>
                     </div>
                 </div>
                 <div class="col-lg-4 col-xs-12">
